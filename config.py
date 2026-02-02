0535efda9704f2d6481a6a7db3926ace051b201a
veer708 <veerahirwal51@gmail.com>
2026-02-02 18:29:47 +0530

    Update configuration with new default values
    Updated environment variables with new default values for BOT_TOKEN, API_ID, API_HASH, ADMINS, DB_URI, DB_NAME, and LOG_CHANNEL.

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 # Don't Remove Credit 
 # Telegram Channel @RexBots_Official
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
-LOG_CHANNEL = -1003656791142
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8212038280:AAEx32u8Qn-Ho1a8maGZBrYlCUORW7OWHqo")
+API_ID = int(os.environ.get("API_ID", "24689889"))
+API_HASH = os.environ.get("API_HASH", "555ff52a3a0b0ef7ad253a6a82ff5c54")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "7521967798").split(",")]
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://veer12730:11rtHhyY2ISxrqzy@cluster0.huleszn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "veer12730")
+LOG_CHANNEL = -1003813251132
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
