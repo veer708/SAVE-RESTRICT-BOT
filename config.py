86307a7c62f727b0cfa52dfb6c77c02606388544
veer708 <veerahirwal51@gmail.com>
2026-02-02 18:44:05 +0530

    Update database URI and name in config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8212038280:AAEx32u8Qn-Ho1a8maGZBrYlCUOR
 API_ID = int(os.environ.get("API_ID", "24689889"))
 API_HASH = os.environ.get("API_HASH", "555ff52a3a0b0ef7ad253a6a82ff5c54")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "7521967798").split(",")]
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://veer12730:11rtHhyY2ISxrqzy@cluster0.huleszn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "veer12730")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://superk7070:DRlvIYSLRSDXzXAL@cluster0.xitnxo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "lucky")
 LOG_CHANNEL = -1003813251132
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
