493b7dbd4eedfd882b0830d7bcb01db94b43f02a
veer708 <veerahirwal51@gmail.com>
2026-02-12 18:02:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8212038280:AAEx32u8Qn-Ho1a8maGZBrYlCUOR
 API_ID = int(os.environ.get("API_ID", "24689889"))
 API_HASH = os.environ.get("API_HASH", "555ff52a3a0b0ef7ad253a6a82ff5c54")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "7521967798").split(",")]
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://superk7070:DRlvIYSLRSDXzXAL@cluster0.xitnxo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://rajraj101930:sXBnmE0VD7KVuFiw@cluster0.pfozrku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "lucky")
 LOG_CHANNEL = -1003813251132
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
